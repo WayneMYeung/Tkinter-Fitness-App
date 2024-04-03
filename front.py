32b9336bf3988fc32b4973891f3042f9f51716b8
CamiC108 <cccathyer@gmail.com>
2024-04-03 17:37:45 -0400

    Added the page 6 for calculation of daily net calories goal

--- front.py
+++ front.py
@@ -8,7 +8,7 @@ class GoalSettingApp(tk.Tk):
         super().__init__()
 
         self.title("Goal Options")
-        self.geometry("560x300")
+        self.geometry("560x320")
 
         self.page = 1  # Current page
         self.gender = tk.StringVar()
@@ -26,6 +26,118 @@ class GoalSettingApp(tk.Tk):
             "Modify Diet Menu": tk.BooleanVar()
         }
 
+        self.protein_list = ["chicken breast", 
+                             "chicken thigh", 
+                             "beef", 
+                             "steak",
+                             "pork", 
+                             "pork belly", 
+                             "fish", 
+                             "eggs", 
+                             "tofu", 
+                             "milk", 
+                             "soy milk", 
+                             "yogurt", 
+                             "nut and seeds", 
+                             "Whey protein powder", 
+                             "cheese" ]
+        self.veggtable_list = [ 
+            "Spinach",
+            "Broccoli",
+            "Carrots",
+            "Tomatoes",
+            "Bell peppers",
+            "Cucumbers",
+            "Lettuce",
+            "Onions",
+            "Garlic",
+            "Potatoes",
+            "Sweet potatoes",
+            "Zucchini",
+            "Eggplant",
+            "Mushrooms",
+            "Celery",
+            "Cabbage",
+            "Radishes",
+            "Beets",
+            "Corn",
+            "Green peas",
+            "Green beans",
+            "Brussels sprouts",
+            "Asparagus",
+            "Artichokes",
+            "Cauliflower",
+            "Kale",
+            "Chard",
+            "Pumpkin",
+            "Squash",
+            "Okra",
+            "Turnips",
+            "Rutabagas",
+            "Fennel",
+            "Bok choy",
+            "Leeks",
+            "Scallions",
+            "Watercress",
+            "Soybeans",
+            "Snap peas",
+            "Bean sprouts",
+            "Jicama",
+            "Kohlrabi",
+            "Yuca",
+            "Parsnips",
+            "Turnip greens",
+            "Collard greens",
+            "Mustard greens",
+            "Swiss chard",
+            "Radish greens",
+            "Endive",
+            "Arugula",
+            "Dandelion greens",
+            "Chicory",
+            "Broccoli rabe",
+            "Sprouts",
+            "Lotus root",
+            "Taro",
+            "Cassava",
+            "Chayote",
+            "Bamboo shoots",
+            "Hearts of palm",
+            "Bitter melon",
+            "Winter melon",
+            "Daikon",
+            "Burdock root",
+            "Jerusalem artichoke",
+            "Fiddlehead ferns",
+            "Samphire",
+            "Lotus root",
+            "Water chestnuts",
+            "Rhubarb",
+            "Tamarind",
+            "Plantains",
+            "Yams",
+            "Cassava",
+            "Yucca",
+            "Arrowroot",
+            "Chicory root",
+            "Ginger",
+            "Turmeric",
+            "Horseradish",
+            "Wasabi",
+            "Salsify",
+            "Skirret",
+            "Yacon",
+            "Chinese artichoke",
+            "Crosne",
+            "Oca",
+            "Maca",
+            "Chicory root",
+            "Sunchoke"
+                    ]
+        self.main_list = []
+        
+        self.inputs = {} # Dictionary to store the inputs from user. 
+
         self.frames = {}  # Dictionary to store frames for each page
 
         self.create_widgets()
@@ -36,23 +148,42 @@ class GoalSettingApp(tk.Tk):
         self.style.configure("TButton", font=("Arial", 14))
         self.style.configure("TEntry", font=("Arial", 14))
 
+        self.navigation_frame = ttk.Frame(self)
+        self.navigation_frame.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+
         if self.page == 1:
             self.create_page1()
-            next_button = ttk.Button(self, text="Next", command=self.next_page)
-            next_button.grid(row=1, column=1, padx=5, pady=10, sticky="e")
+            self.show_next_button()
             self.hide_back_button()
+            self.hide_submit_button()
         elif self.page == 2:
             self.create_page2()
             self.show_back_button()
+            self.show_next_button()
+            self.hide_submit_button()
         elif self.page == 3:
             self.create_page3()
+            self.show_next_button()
             self.show_back_button()
+            self.hide_submit_button()
         elif self.page == 4:
             self.create_page4()
+            self.show_next_button()
             self.show_back_button()
+            self.hide_submit_button()
         elif self.page == 5:
             self.create_page5()
+            self.show_submit_button()
             self.show_back_button()
+            self.hide_next_button()
+        elif self.page == 6:
+            self.create_page6()
+            self.hide_submit_button()
+            self.show_next_button()
+            self.show_back_button()
+        
+        self.update_navigation_buttons()
+
 
     def create_page1(self):
         self.frames[1] = ttk.Frame(self)
@@ -161,8 +292,65 @@ class GoalSettingApp(tk.Tk):
         for idx, option in enumerate(activity_options):
             ttk.Radiobutton(self.frames[5], text=option, variable=self.activity_level, value=option).grid(row=idx + 1, column=0, padx=5, pady=2, sticky="w")
 
+    def create_page6(self):
+        self.frames[6] = ttk.Frame(self)
+        self.frames[6].grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
+        self.get_inputs()
+
+        goal_label = ttk.Label(self.frames[6], text="Your Daily Net Calories Result:", font=("Arial", 18))
+        goal_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+        result_calories = self.cal_calories_goal()
+        result_label = ttk.Label(self.frames[6], text=f"You burn {result_calories} calories during a typical day", font=("Arial", 20))
+        result_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+
+        paragraph_text = "Remember that this estimate is based on your body weight, height, age, gender, and your average level of activity. You can use this information to help you figure out how many calories you should be consuming to maintain your weight."
+        paragraph_label = ttk.Label(self.frames[6], text=paragraph_text, font=("Arial", 14), wraplength=500, justify="left")
+        paragraph_label.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
+    
+    def create_page7(self):
+        self.frames[7] = ttk.Frame(self)
+        self.frames[7].grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
+        goal_label = ttk.Label(self.frames[7], text="What is your breakfast intake", font=("Arial", 18))
+        goal_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+        
+        breakfast_protein = ttk.Combobox(self.frames[7], values=["Feet/Inches", "Centimeters"],
+                                         textvariable=self.height_unit, state="readonly")
+
+
+
+
     def next_page(self):
-        if self.page < 5:
+        if self.page == 1:
+            # Check if at least one goal is selected
+            if not any(var.get() for var in self.goals.values()):
+                messagebox.showwarning("No Goal Selected", "Please select at least one goal.")
+                return
+        elif self.page == 2:
+            # Check if gender and age are selected
+            if not self.gender.get() or not self.age.get():
+                messagebox.showwarning("Missing Information", "Please select gender and enter age.")
+                return
+        elif self.page == 3:
+            # Check if height unit and value are selected
+            if not self.height_unit.get() or not self.height_value.get():
+                messagebox.showwarning("Missing Information", "Please select height unit and enter height.")
+                return
+        elif self.page == 4:
+            # Check if weight and goal weight are entered
+            if not self.weight.get() or not self.goal_weight.get():
+                messagebox.showwarning("Missing Information", "Please enter weight and goal weight.")
+                return
+        elif self.page == 5:
+            # Check if activity level is selected
+            if not self.activity_level.get():
+                messagebox.showwarning("Missing Information", "Please select activity level.")
+                return
+            
+        if self.page < 6:
             self.page += 1
             self.create_widgets()
 
@@ -184,16 +372,73 @@ class GoalSettingApp(tk.Tk):
             messagebox.showwarning("Invalid Input", f"{field.capitalize()} should contain only numbers.")
             return False
 
+    def update_navigation_buttons(self):
+        if self.page == 5:
+            self.next_button.config(state=tk.DISABLED)
+
+
     def hide_back_button(self):
         for widget in self.winfo_children():
             if isinstance(widget, ttk.Button) and widget["text"] == "Back":
                 widget.grid_forget()
+    
+    def hide_submit_button(self):
+         for widget in self.winfo_children():
+            if isinstance(widget, ttk.Button) and widget["text"] == "Submit":
+                widget.grid_forget()
+    
+    def hide_next_button(self):
+         for widget in self.winfo_children():
+            if isinstance(widget, ttk.Button) and widget["text"] == "Next":
+                widget.grid_forget()
+    
+    def show_next_button(self): 
+        self.next_button = ttk.Button(self.navigation_frame, text="Next", command=self.next_page)
+        self.next_button.grid(row=1, column=4, padx=10, pady=10, sticky="nsew")
 
     def show_back_button(self):
-        back_button = ttk.Button(self, text="Back", command=self.prev_page)
-        back_button.grid(row=1, column=0, padx=5, pady=10, sticky="w")
+        self.back_button = ttk.Button(self.navigation_frame, text="Back", command=self.prev_page)
+        self.back_button.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+    
+    def show_submit_button(self):
+        self.submit_button = ttk.Button(self.navigation_frame, text="Submit", command=self.get_inputs)
+        self.submit_button.grid(row=1, column=5, padx=10, pady=10, sticky="nsew")
 
+    def get_inputs(self):
+        self.next_page()
+        self.inputs["gender"] = app.gender.get()
+        self.inputs["age"] = float(app.age.get())
+        self.inputs["height_unit"] = app.height_unit.get()
+        if self.inputs["height_unit"] == "Feet/Inches":
+            self.inputs["height_value"] = float(app.height_value.get()) * 2.54
+        else:
+            self.inputs["height_value"] = float(app.height_value.get())
+        self.inputs["weight"] = float(app.weight.get())
+        self.inputs["goal_weight"] = float(app.goal_weight.get())
+        self.inputs["activity_level"] = app.activity_level.get()
+
+        self.next_page()
+
+    def cal_calories_goal(self):
+        calories = 0
+        if self.inputs["gender"] == "Female":
+            calories = 65.51 + (9.563 * self.inputs["weight"]) + (1.850 * self.inputs["height_value"]) - (4.676 * self.inputs["age"])
+        elif self.inputs["gender"] == "Male":
+            calories = 66.47 + (13.75 * self.inputs["weight"]) + (5.003 * self.inputs["height_value"]) - (6.755 * self.inputs["age"])
+
+        if self.inputs["activity_level"] == "Sedentary (little to no exercise)":
+            calories *= 1.2
+        elif self.inputs["activity_level"] == "Lightly active (light exercise/sports 1-3 days a week)":
+            calories *=1.375
+        elif self.inputs["activity_level"] == "Moderately active (moderate exercise/sports 3-5 days a week)":
+            calories *= 1.55
+        elif self.inputs["activity_level"] == "Very active (hard exercise/sports 6-7 days a week)":
+            calories *= 1.725
+        elif self.inputs["activity_level"] == "Extra active (very hard exercise/sports & physical job or 2x training)":
+            calories *= 1.9
+        return calories
 
 if __name__ == "__main__":
     app = GoalSettingApp()
     app.mainloop()
+
